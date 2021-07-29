a4f6344f35b86d65fdd2474d25982b8f12e06679
YiroYi <yiroyi@live.com>
2021-07-29 22:40:07 +0900

    Adding Arguments to the URL

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { GraphQLServer } from "graphql-yoga";
 import axios from "axios";
 
+const db = 'http://localhost:3004'
+
 const server = new GraphQLServer({
   typeDefs: `
     type Query {
       agent(id:ID!): User!
-      agents: [User]
+      agents(name:String, age:Int): [User!]!
+      cars: [String]
+      msg(values: [String!]): String
     } 
 
     type User {
@@ -19,13 +23,26 @@ const server = new GraphQLServer({
   resolvers: {
     Query: {
       agent: async (parent, args, context, info) => {
-        const response = await axios.get(`http://localhost:3004/users/${args.id}`);
+        const response = await axios.get(`${db}/users/${args.id}`);
         return response.data;
       },
-      agents: async () => {
-        const response = await axios.get("http://localhost:3004/users");
+      agents: async (parents, args, context, info) => {
+        const name = args.name != null ? `name=${args.name}` : '';
+        const age = args.age != null ? `age=${args.age}` : '';
+
+
+        const response = await axios.get(`${db}/users?${name}&${age}`);
         return response.data;
       },
+      cars: () => {
+        return ['Honda', 'Mazda', null]
+      },
+      msg: (parent, args, context, info) => {
+        if(args.values.length === 0){
+          return 'No values'
+        }
+        return `hello ${args.values[0]} ${args.values[1]}`
+      }
     },
   },
 });
