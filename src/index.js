d6bae063c449088843b3a16917aa1cd4b6ba0312
YiroYi <yiroyi@live.com>
2021-07-29 15:13:23 +0900

    Adding scalar type

--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import { GraphQLServer } from "graphql-yoga";
 const server = new GraphQLServer({
   typeDefs: `
     type Query {
-      start: String!
+      id: ID!
+      name: String!
+      age: Int
+      married: Boolean
+      average: Float
     }
   `,
   resolvers: {
     Query: {
-      start() {
-        return "We are starting";
+      id() {
+        return 10;
+      },
+      name() {
+        return "Yiro";
+      },
+      age() {
+        return 34;
+      },
+      married() {
+        return false;
+      },
+      average() {
+        return 9.8;
       },
     },
   },
